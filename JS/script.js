f736d3d6731ba710997445319751964f34ebe744
Orynik <Dkrovel@gmail.com>
2019-06-26 13:43:53 +0800

    Revert "some fix slider xs"
    This reverts commit 00976bcd244103bab327b4938eeeb8ab46599fe9.

--- JS/script.js
+++ JS/script.js
@@ -54,14 +54,14 @@ trigger.onclick = function () {
 }
 
 overflow.onclick = function () { //Клик вне области
-    modal.style.top = "-170%";
+    modal.style.top = "-150%";
 	wrapper.classList.remove("wrapper_blur"); //Удаление размытия
     overflow.remove();
     body.style.overflow = "scroll";
 }
 
 closeBtn.addEventListener('click', function(){ //Клик по кнопке закрытия
-	modal.style.top = "-170%";
+	modal.style.top = "-150%";
 	wrapper.classList.remove("wrapper_blur") //Удаление размытия
     overflow.remove();
     body.style.overflow = "scroll";
