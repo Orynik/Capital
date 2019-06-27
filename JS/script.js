e0580462ea0ea7f0a9c7a30110a2be25c3e2c884
Orynik <Dkrovel@gmail.com>
2019-06-28 04:22:45 +0800

    garbage cleaning and adding comments

--- JS/script.js
+++ JS/script.js
@@ -60,21 +60,23 @@ scrap.onclick = function(){
     calculate(input_value,calc_probe,calc_quality);
 }
 
-
+//Удаление active с других кнопок и дбавление на нажатую
 function remove_active_simple(elem){
-    document.querySelectorAll('.buttons-probe .active').forEach(n => n.classList.remove('active')) //Удаляет класс active у дочерних элементов buttons-probe
+    document.querySelectorAll('.buttons-probe .active').forEach(n => n.classList.remove('active')) 
     elem.classList.add("active");
 }
-
-input.oninput = function() {
-    input_value = input.value; //Запись изменения поля input
-    calculate(input_value,calc_probe,calc_quality);
-};
 
 function remove_active_quality(elem){
     document.querySelectorAll('.buttons-quality .active').forEach(n => n.classList.remove('active'))
     elem.classList.add("active");
 }
+
+//Вывод данных с input
+input.oninput = function() {
+    input_value = input.value;
+    calculate(input_value,calc_probe,calc_quality);
+};
+
 
 function calculate (weight, probe, quality){
     var calc_result = weight * probe * quality;
@@ -118,68 +120,69 @@ prew.onclick = function () {
 }
 
 //Слайдер поступлений
-	var next_item = document.getElementById("next-items");
-	var prew_item = document.getElementById("prew-items");
-    var slider_items = document.querySelector(".slider-items");
-    
-    //Ширина видимого блока
-    var width_visibity_block = document.querySelector(".slider-out").offsetWidth; 
+var next_item = document.getElementById("next-items");
+var prew_item = document.getElementById("prew-items");
+var slider_items = document.querySelector(".slider-items");
 
-    //Буффер для использования в вычислениях
-    var buffer = width_visibity_block; 
+//Ширина видимого блока
+var width_visibity_block = document.querySelector(".slider-out").offsetWidth; 
 
-    //Ширина одного слайд-блока
-    var width_slide = document.querySelector(".items-wrapper").offsetWidth; 
-    var swipe = 0; 
-    //Ширина скрытого блока    
-    const width_slider = document.querySelector(".slider-items").offsetWidth;
+//Буффер для использования в вычислениях
+var buffer = width_visibity_block; 
 
-    //Котнроль ширин
-    console.log("Видимый блок: " + width_visibity_block);
-    console.log("Один слайд-блок:" + width_slide);
-    console.log("Скрытый блок:" + width_slider);
+//Ширина одного слайд-блока
+var width_slide = document.querySelector(".items-wrapper").offsetWidth; 
+var swipe = 0; 
 
-	slider_items.style.transform = "translate3d(-0px, 0px, 0px)";
-	slider_items.style.transition = "all 1s ease 0s";
+//Ширина скрытого блока
+const width_slider = document.querySelector(".slider-items").offsetWidth;
 
-    console.log("Сумма видимого блока и слайд-блока: " + (buffer + width_slide));
-	
-	next_item.onclick = function(){
-		if(buffer != width_slider){
-			swipe += width_slide;
-			slider_items.style.transform = "translate3d( -" + swipe + "px, 0px, 0px)";
-			buffer += width_slide;
-			prew_item.classList.remove("disabled");
-            next_item.classList.remove("disabled");
-            if(buffer == width_slider){
-                prew_item.classList.remove("disabled");
-                next_item.classList.add("disabled");
-            }
-		}
+//Котнроль ширин
+console.log("Видимый блок: " + width_visibity_block);
+console.log("Один слайд-блок:" + width_slide);
+console.log("Скрытый блок:" + width_slider);
+
+slider_items.style.transform = "translate3d(-0px, 0px, 0px)";
+slider_items.style.transition = "all 1s ease 0s";
+
+next_item.onclick = function(){
+	if(buffer != width_slider){
+		swipe += width_slide;
+		slider_items.style.transform = "translate3d( -" + swipe + "px, 0px, 0px)";
+		buffer += width_slide;
+		prew_item.classList.remove("disabled");
+        next_item.classList.remove("disabled");
+        if(buffer == width_slider){
+            prew_item.classList.remove("disabled");
+            next_item.classList.add("disabled");
+        }
 	}
-    
-	prew_item.onclick = function(){
-		if(swipe != 0){
-            swipe -= width_slide;
-			slider_items.style.transform = "translate3d(-"+ swipe +"px, 0px, 0px)"
-            buffer -= width_slide;
-			prew_item.classList.remove("disabled");
+}
+
+prew_item.onclick = function(){
+	if(swipe != 0){
+        swipe -= width_slide;
+		slider_items.style.transform = "translate3d(-"+ swipe +"px, 0px, 0px)"
+        buffer -= width_slide;
+		prew_item.classList.remove("disabled");
+        next_item.classList.remove("disabled");
+        if(buffer == width_visibity_block){
             next_item.classList.remove("disabled");
-            if(buffer == width_visibity_block){
-                next_item.classList.remove("disabled");
-                prew_item.classList.add("disabled")
-            }
-		}	
-    }
+            prew_item.classList.add("disabled")
+        }
+	}	
+}
 
 //Модальное окно
 
 var closeBtn = document.querySelector('.closeBtn');
 var modal = document.querySelector('.modal');
 var overflow = document.createElement('div');
+
 // !!!! класс для добавления блюра
 var wrapper = document.querySelector('.blur');
 // !!!!
+
 var body = document.querySelector('body');
 var trigger = document.querySelector('.details-location');
 trigger.onclick = function () {
@@ -194,7 +197,6 @@ overflow.onclick = function () { //Клик вне области
     modal.style.top = "-170%";
 	wrapper.classList.remove("wrapper_blur"); //Удаление размытия
     overflow.remove();
-    body.style.overflow = "scroll";
 }
 
 closeBtn.addEventListener('click', function(){ //Клик по кнопке закрытия
